367ecca7808ef61719c8f3aa6c9a4032d4ca8749
DomenickD <deothe1@gmail.com>
2024-09-04 09:54:22 -0400

    test 2 successful. New baseline

--- pages/page_1.py
+++ pages/page_1.py
@@ -1,3 +1,5 @@
+"""This page will have information on the data with graphs, what model and what model was used. """
+
 import streamlit as st
 
 st.title("test page 1")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
+"""Main splash page for teh streamlit app"""
+
 import streamlit as st
 
 st.title("test main page")
 
-st.write("Test successful. Update one test for sync time. ")
+st.write("Test 2 successful.")
