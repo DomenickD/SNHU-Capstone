85a2b345be286b96ca8db19d322096ecab3a45c1
DomenickD <deothe1@gmail.com>
2024-09-05 14:02:09 -0400

    page3, lint update

---
+++ pages/page_3.py
@@ -0,0 +1,13 @@
+"""Conclusion page"""
+
+import streamlit as st
+
+st.title("Special Thanks")
+
+st.write(
+    "I wanted to give a special thanks to those who have helped me make it this far."
+)
+
+st.write(
+    "Thank you Diedre Wilhite - for all the support and being teh best advisor ever!"
+)
