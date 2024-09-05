0f359fe7ab41c791f6656d7625d3fc00e4858810
DomenickD <deothe1@gmail.com>
2024-09-05 13:02:24 -0400

    streamlit display test 3

--- pages/page_1.py
+++ pages/page_1.py
@@ -1,16 +1,17 @@
 """This page will have information on the data with graphs, what model and what model was used. """
 
 import streamlit as st
-import yfinance as yf
+
+# import yfinance as yf
 
 st.title("test page 1")
 
 
-# Define the ticker symbol
-ticker = yf.Ticker("AAPL")
+# # Define the ticker symbol
+# ticker = yf.Ticker("AAPL")
 
-# Get historical data for the last year
-historical_data = ticker.history(period="1y")
+# # Get historical data for the last year
+# historical_data = ticker.history(period="1y")
 
-# Display the DataFrame
-st.write(historical_data)
+# # Display the DataFrame
+# st.write(historical_data)
