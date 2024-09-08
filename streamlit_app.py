0c5e443df18f260bb5b3fb83b76de6f8e36549fc
DomenickD <deothe1@gmail.com>
2024-09-08 13:09:08 -0400

    update splash page

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,20 @@
 
 import streamlit as st
 
-st.title("test main page")
+st.title("Predicting Stock data for FAANG stocks")
 
-st.write("Test 2 successful.")
+st.caption("By: Domenick Dobbs")
+
+st.caption("Data obtained from the yfinance API.")
+
+st.divider()
+
+st.subheader("What is FAANG?")
+st.write(
+    "FAANG is an acronym referring to five leading tech companies: \
+         Facebook (Meta), Amazon, Apple, Netflix, and Google (Alphabet). \
+         These companies are influential in the stock market due to their \
+         large market share, growth potential, and popularity with investors. \
+         However, like any investment, FAANG stocks carry risks, and it's \
+         important to conduct thorough research before investing."
+)
